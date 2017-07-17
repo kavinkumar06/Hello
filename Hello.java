c95b850c18fd162f38cb117eda8ce2800dea3681
kavinkumar06 <kavinm09@gmail.com>
2017-07-17 14:16:18 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+# Hello
+import java.io.*;
+import java.util.*;
+public class Hello
+{
+  public static void main(String args[])
+  {
+    int i = 0;
+        if(System.out.printf("Sanfoundary!! ") == null) {}
+        for(i = 1; i < 2; System.out.println("Hello World."))
+        {
+            i++;
+     	}
+     }
+}
